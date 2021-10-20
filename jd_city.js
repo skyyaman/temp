469563c2d0e44d5c0f020c906da9109678096d7f
skyyaman <skyyaman@163.com>
2021-10-20 10:17:59 +0800

    Update jd_city.js

--- jd_city.js
+++ jd_city.js
@@ -34,7 +34,7 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //let inviteCodes = ['RtGKzeX2SQigedLMFddn1cwQcppajCK5h_oaMK09uxa1GVIdcQ@W9GzoJjSI0PrL-PvVLx_mjHeuJbxWx_0uScsLnKcvJ3BSyQ']
-let inviteCodes = ['']
+let inviteCodes = ['XX@XX']
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
